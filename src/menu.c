58801c9a8144f172ff6983610df9797dd6f6ed21
assen <assen@31e3ab36-42c6-415d-bc2c-92f8adfe3392>
2013-11-03 18:25:33 +0000

    Finished adding CentOS

--- src/menu.c
+++ src/menu.c
@@ -130,7 +130,7 @@ void menu_cb_settings (GtkAction *action, livescore_applet *applet) {
 
 	// Prepare Feed tab
 	GtkWidget *feed_label = gtk_label_new(_("Choose which feed provider to use:\n(Changing the provider will erase your notification settings)"));
-	gtk_label_set_justify(feed_label, GTK_JUSTIFY_CENTER);
+	gtk_label_set_justify(GTK_LABEL(feed_label), GTK_JUSTIFY_CENTER);
 
 #ifdef HAVE_MATE
 	GtkWidget *feed_combo = gtk_combo_box_text_new();
