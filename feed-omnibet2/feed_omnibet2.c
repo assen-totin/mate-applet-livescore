0f50118656f3053fc53e3aad91af13618657de9a
Assen Totin <assen.totin@gmail.com>
2017-04-23 17:17:16 +0300

    Fix timezone.

--- feed-omnibet2/feed_omnibet2.c
+++ feed-omnibet2/feed_omnibet2.c
@@ -197,8 +197,8 @@ time_t omnibet_convert_time(char *s) {
 	// mktime treats 'now' as localtime, but we have it UTC.
 	// NOTE: timegm() is not POSIX-compliant, hence not portable!
 	// See 'man timegm' for POSIX-compliant replacecement function.
-	//return mktime(&now);
-	return timegm(&now);
+	return mktime(&now);
+	//return timegm(&now);
 }
 
 void omnibet_build_match(omnibet_match_data *omnibet_match, match_data **feed_matches, int *feed_matches_counter) {
